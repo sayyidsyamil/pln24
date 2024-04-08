6b7bad13e8c59d33b085444ce6423b90fc791d5f
sayyidsyamil <102892516+sayyidsyamil@users.noreply.github.com>
2024-04-08 16:34:07 +0800

    Update rules.tsx

--- components/ui/rules.tsx
+++ components/ui/rules.tsx
@@ -3,23 +3,31 @@ import { EvervaultCardDemo } from '../EvervaultCardDemo';
 
 const Rules = () => {
     return (
-        <div className="grid md:grid-cols-4 grid-cols-2 gap-5 min-w-screen">
-            <EvervaultCardDemo
-                cardText="Team Composition"
-                hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
-            />
-            <EvervaultCardDemo
-                cardText="Registration"
-                hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
-            />
-            <EvervaultCardDemo
-                cardText="Contest Structure"
-                hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
-            />
-            <EvervaultCardDemo
-                cardText="Preliminary Round"
-                hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
-            />
+        <div className="grid md:grid-cols-4 grid-cols-2 gap-5 w-full">
+            <div className="min-w-0">
+                <EvervaultCardDemo
+                    cardText="Team Composition"
+                    hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
+                />
+            </div>
+            <div className="min-w-0">
+                <EvervaultCardDemo
+                    cardText="Registration"
+                    hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
+                />
+            </div>
+            <div className="min-w-0">
+                <EvervaultCardDemo
+                    cardText="Contest Structure"
+                    hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
+                />
+            </div>
+            <div className="min-w-0">
+                <EvervaultCardDemo
+                    cardText="Preliminary Round"
+                    hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
+                />
+            </div>
         </div>
     );
 };
