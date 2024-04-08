6de7fa16c802b84c8acb9d85792f008a1d039af8
sayyidsyamil <102892516+sayyidsyamil@users.noreply.github.com>
2024-04-08 16:28:46 +0800

    Update rules.tsx
    used grid instead of flex

--- components/ui/rules.tsx
+++ components/ui/rules.tsx
@@ -3,28 +3,23 @@ import { EvervaultCardDemo } from '../EvervaultCardDemo';
 
 const Rules = () => {
     return (
-        <div className='flex flex-col gap-5 lg:flex-row'>
-            <div className='flex flex-row gap-5'>
-                <EvervaultCardDemo
-                    cardText="Team Composition"
-                    hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
-                />
-                <EvervaultCardDemo
-                    cardText="Registration"
-                    hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
-                />
-            </div>
-            <div className='flex flex-row gap-5'>
-                <EvervaultCardDemo
-                    cardText="Contest Structure"
-                    hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
-                />
-
-                <EvervaultCardDemo
-                    cardText="Preliminary Round"
-                    hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
-                />
-            </div>
+        <div className="grid md:grid-cols-4 grid-cols-2 gap-5">
+            <EvervaultCardDemo
+                cardText="Team Composition"
+                hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
+            />
+            <EvervaultCardDemo
+                cardText="Registration"
+                hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
+            />
+            <EvervaultCardDemo
+                cardText="Contest Structure"
+                hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
+            />
+            <EvervaultCardDemo
+                cardText="Preliminary Round"
+                hoverText="Hover over this card to reveal an awesome effect. Running out of copy here."
+            />
         </div>
     );
 };
