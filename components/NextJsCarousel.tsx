8a8c5bd500329d327dc95a7ed22addf901ac6fb8
sayyidsyamil <102892516+sayyidsyamil@users.noreply.github.com>
2024-04-15 06:05:29 +0800

    Update NextJsCarousel.tsx

--- components/NextJsCarousel.tsx
+++ components/NextJsCarousel.tsx
@@ -8,16 +8,10 @@ const NextJsCarousel = () => {
     <div className='flex justify-center items-center text-center'>
       <Carousel className='max-w-screen-lg w-full overflow-hidden'>
         <div className="aspect-video bg-black">
-          <Image src="/announcement1.PNG" alt="image0" width={1980} height={1080} className="w-full h-full object-contain" />
+          <Image src="/announcement1.PNG" alt="image1" width={1980} height={1080} className="w-full h-full object-contain" />
         </div>
         <div className="aspect-video bg-black">
-          <Image src="/announcement2.png" alt="image1" width={1980} height={1080} className="w-full h-full object-contain" />
-        </div>
-        <div className="aspect-video bg-black">
-          <Image src="/announcement3.png" alt="image3" width={1980} height={1080} className="w-full h-full object-contain" />
-        </div>
-        <div className="aspect-video bg-black">
-          <Image src="/announcement4.png" alt="image4" width={1980} height={1080} className="w-full h-full object-contain" />
+          <Image src="/announcement2.PNG" alt="image2" width={1980} height={1080} className="w-full h-full object-contain" />
         </div>
       </Carousel >
     </div >
