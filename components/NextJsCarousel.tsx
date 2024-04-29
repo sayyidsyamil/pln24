9b1b3ff4989befafaadbf25afa2aac30730588cc
sayyidsyamil <102892516+sayyidsyamil@users.noreply.github.com>
2024-04-29 19:39:19 +0800

    Update NextJsCarousel.tsx

--- components/NextJsCarousel.tsx
+++ components/NextJsCarousel.tsx
@@ -8,6 +8,9 @@ const NextJsCarousel = () => {
     <div className='flex justify-center items-center text-center'>
       <Carousel className='max-w-screen-lg w-full overflow-hidden'>
         <div className="aspect-video bg-black">
+          <Image src="/announcement4.jpg" alt="image1" width={1980} height={1080} className="w-full h-full object-contain" />
+        </div>
+        <div className="aspect-video bg-black">
           <Image src="/announcement3.png" alt="image1" width={1980} height={1080} className="w-full h-full object-contain" />
         </div>
         <div className="aspect-video bg-black">
